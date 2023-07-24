475edf7ebed02f603ce6a642f7664a7202b369bb
gginglis <inglisge23@craighead.school.nz>
2023-07-25 10:44:01 +1200

    did the first bit

---
+++ script.js
@@ -0,0 +1,14 @@
+let pets = [
+    'your',
+    'mum',
+    'haha'
+]
+function petRegister() {
+    let huh = prompt('give me the name')
+    pets.push(huh)
+}
+{
+    function showPets() {
+        alert(pets)
+    }
+}
