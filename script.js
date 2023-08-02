2a6e2205bef26154deb2b97faf0461314e4d6b49
gginglis <inglisge23@craighead.school.nz>
2023-08-03 10:52:03 +1200

    added the show pets function
    tested it by showing the array and adding a pet and showing it again

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 let pets = [
-    'your',
-    'mum',
-    'haha'
+    { name: 'Max', age: 1, species: 'Doggo' },
+    { name: 'Mary', age: 2, species: 'Doggo' },
+    { name: 'MacDonald', age: 3, species: 'Catterpus' },
+    { name: 'McNaught', age: 4, species: 'Cyclops' },
 ]
-function petRegister() {
-    let huh = prompt('give me the name')
-    pets.push(huh)
-}
-{
-    function showPets() {
-        alert(pets)
+function addPet() {
+    let name = prompt('give me the name')
+    let age = prompt('give me the age')
+    let species = prompt('give me the species')
+    let pet = {
+        name: name,
+        age: age,
+        species: species,
     }
+    pets.push(pet)
+}
+
+function showPets() {
+    // loop through the pet array and for each pet, alert the name
+    for (let pet of pets) {
+        alert(pet.name)
+    }
+}
+
+function removePet() {
+    // output all the pets (same as showPets())
+
+    // get the index of the pet to remove from the user
+
+    // delete that index of the array
+}
+
+function searchPets() {
+    // get the name of the pet to search for from the user
+
+    // create a variable to store the result of the search, starting off as 'false' for 'haven't found it yet'
+
+    // for each pet in the array, check if the name of the pet is the same as the one the user is searching for
+
+    // if it is the same, update the result of the search to 'true'
+
+    // if the result of the search is true, tell the user the pet was found
+
+    // if the result of the search is false, tell the user the pet isn't in the register
 }
